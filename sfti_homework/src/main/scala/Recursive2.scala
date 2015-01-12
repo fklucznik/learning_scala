fa30ebaa78c6c2761d1cb24acad5361803a9353d
fklucznik <frank.klucznik@gtri.gatech.edu>
2015-01-12 07:30:51 -0500

    v6

--- sfti_homework/src/main/scala/Recursive2.scala
+++ sfti_homework/src/main/scala/Recursive2.scala
@@ -34,7 +34,22 @@ object Recursive2 extends App {
 
 
   }
-  power(5, 4)
+  //power(5, 4)
+
+  def power2(x: Double, n: Int) : Double = {
+
+    n match {
+      case 0 => 1.0
+      case nn if nn < 0 =>
+        power2(1.0 / x, nn * -1)
+      case nn if nn % 2 == 0 =>
+        val y = power2(x, nn / 2)
+        y * y
+      case _ =>
+        x * power2(x, n - 1)
+    }
+  }
+  println(power2(5.0, 3))
 }
 
 
