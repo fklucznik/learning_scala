4734cd0315c884e35138e43c846550801ac4ed9f
fklucznik <frank.klucznik@gtri.gatech.edu>
2015-01-17 08:00:46 -0500

    v8

--- sfti/src/main/scala/org/s_mach/frank_w_klucznik/Chapter3.scala
+++ sfti/src/main/scala/org/s_mach/frank_w_klucznik/Chapter3.scala
@@ -20,12 +20,10 @@ object Chapter3 {
 
     // Write a loop that swaps adjacent elements of an array of integers
 
+    //Generate array
     var a = Array(66,67,68,69,70,71,72,73)
 
-    for (x <- a) {
-      println(x)
-    }
-
+    //Loop
     var count = 0
     for (count <- 0 until (a.size, 2)) {
 
@@ -38,11 +36,9 @@ object Chapter3 {
         //Reassign Values
         a(count) = second
         a(count + 1) = first
-
       }
-
     }
-
+    //Display results
     for (x <- a) {
       println(x)
     }
@@ -53,21 +49,87 @@ object Chapter3 {
     // Write a loop that swaps adjacent elements of an array of integers
     // Use for/yield
 
-    import scala.collection._
-    object Array1 extends App {
+    //Generate array
+    val a = Array(66,67,68,69,70,71,72,73,74,80)
 
-      var a = Array(66,67,68,69,70,71,72,73,74,80)
+    //Generate indexed sequence of arrays
+    val result: IndexedSeq[Array[Int]] = for (count <- 0 until (a.size, 2)) yield
+    {if ((count + 1) < a.size) Array(a(count + 1), a(count)) else Array(a(count))}
 
-      for (x <- a) {
-        println(x)
-      }
+    //Flatten the indexed sequence
+    val result2: IndexedSeq[Int] = result.flatten
 
-      val result = for (count <- 0 until (a.size, 2)) yield
-      {if ((count + 1) < a.size) (a(count + 1), a(count)) else a(count)}
+    //Convert indexed sequence to an array for the correct answer
+    val answer: Array[Int] = result2.toArray
+  }
 
-      for (x <- result) {
-        println(x)
-      }
+  def question4() :Unit = {
+    //Produce a new array containing all positive values in their original order,
+    //followed by all values zero or negative in their original order
+
+    //Generate Array
+    val a = Array(66,-67,68,-9,70,71,-72,73,-74,80)
+
+    //Save positive indexes
+    val posIndexes = for(i <- 0 until a.size if (a(i) > 0)) yield {
+      i
     }
+
+    //Save negative indexes
+    val negIndexes = for(i <- 0 until a.size if (a(i) <= 0)) yield {
+      i
+    }
+    //Create a new array
+    var b = new Array[Int](a.size)
+
+
+    //Add positive numbers to new array
+    for (i <- 0 until posIndexes.size) b(i) = a(posIndexes(i))
+
+    //Add negative numbers to new array
+    for (j <- 0 until negIndexes.size) b(posIndexes.size + j) = a(negIndexes(j))
+
+  }
+
+
+  def question5() :Unit = {
+    //How do you computer the average of an Array[Double]?
+
+    val a = Array(66.6, 77.7, 22.0, -44.4, 29.2)
+
+    a.sum/(a.size +1)
+
+  }
+
+
+  def   question6() :Unit = {
+    //How do you rearrange the elements of an Array[Int] so they appear in reverse sorted order?
+    //How do you do the same w/ an ArrayBuffer[Int]?
+
+
+    val a = Array(66.6, 77.7, 22.0, -44.4, 29.2)
+    val b = a.reverse
+
+    import scala.collection.mutable.ArrayBuffer
+    val a = ArrayBuffer(66, 77, 22, -44, 29, 0)
+
+    val b = a.reverse
+
+  }
+
+  def question7() :Unit = {
+    //Write a code snippet that produces all values from an array w/ duplicates removed
+
+    var a = Array(66,67,68,69,70,71,72,72,73,74,80)
+    a.distinct
+
+  }
+
+  def question8() :Unit = {
+    //Rewrite the example at the end of Section 4 using teh drop method for dropping
+    //the index of the first match.
+
+
+
   }
 }
