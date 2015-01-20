c473eebeff0bb31f88c277852c6ec119d9798523
fklucznik <frank.klucznik@gtri.gatech.edu>
2015-01-19 22:15:42 -0500

    v11

--- sfti/src/main/scala/org/s_mach/frank_w_klucznik/Chapter3.scala
+++ sfti/src/main/scala/org/s_mach/frank_w_klucznik/Chapter3.scala
@@ -161,4 +161,46 @@ object Chapter3 {
 
 
   }
+
+  def  question9() :Unit = {
+    /** Make a collection of all timezones that are in America
+      * Strip off the "America/" prefix ans sort the result
+      */
+    import java.util.TimeZone
+
+    object Timezone {
+      //Get available timezone IDs
+      var AvailableIds: Array[String] = TimeZone.getAvailableIDs()
+
+      //Capture index of all items in list "America/" prefix in order
+      var Indexes: IndexedSeq[Int] = for (i <- 0 until AvailableIds.size if (AvailableIds(i).startsWith("America/"))) yield {
+        i
+      }
+
+      //Output list before stripping prefix
+      for (i <- 0 until Indexes.size) {
+        println(AvailableIds(Indexes(i)))
+      }
+
+      //Strip prefix "America/"
+      var Result: IndexedSeq[String] = for(i <- 0 until Indexes.size) yield { AvailableIds(Indexes(i)).stripPrefix("America/")}
+
+      //Sort the list
+      Result = Result.sorted
+
+      //Display resulting list
+      for(x <- Result){
+        println(x)
+      }
+
+    }
+  }
+
+  def question10() :Unit = {
+
+
+
+
+
+  }
 }
--- sfti_homework/src/main/scala/Timezone.scala
+++ sfti_homework/src/main/scala/Timezone.scala
@@ -4,15 +4,26 @@ import java.util.TimeZone
  * Created by frank on 1/19/15.
  */
 object Timezone extends App {
-
+  //Get available timezone IDs
   var AvailableIds: Array[String] = TimeZone.getAvailableIDs()
 
   //Capture index of all items in list "America/" prefix in order
-  var Indexes = for(i <- 0 until AvailableIds.size if (AvailableIds(i).startsWith("America/"))) yield {i}
+  var Indexes: IndexedSeq[Int] = for(i <- 0 until AvailableIds.size if (AvailableIds(i).startsWith("America/"))) yield {i}
 
-
-  for(i <- 0 until Indexes.size){
+  //Output list before stripping prefix
+  for(i <- 0 until Indexes.size) {
     println(AvailableIds(Indexes(i)))
+  }
+
+  //Strip prefix "America/"
+  var Result: IndexedSeq[String] = for(i <- 0 until Indexes.size) yield { AvailableIds(Indexes(i)).stripPrefix("America/")}
+
+  //Sort the list
+  Result = Result.sorted
+
+  //Display resulting list
+  for(x <- Result){
+    println(x)
   }
 
 }
