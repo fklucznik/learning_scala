d2995fe1a66b7e9a99f1ac135511ec3ed804c503
fklucznik <frank.klucznik@gtri.gatech.edu>
2015-02-07 23:59:00 -0500

    v14

--- sfti/src/main/scala/org/s_mach/frank_w_klucznik/Chapter4.scala
+++ sfti/src/main/scala/org/s_mach/frank_w_klucznik/Chapter4.scala
@@ -1,5 +1,10 @@
 package org.s_mach.frank_w_klucznik
 
+import _root_.java.io.File
+import _root_.java.io.File
+import _root_.java.util.Scanner
+import _root_.java.util.Scanner
+
 /**
  * Created by frank on 1/20/15.
  */
@@ -24,35 +29,129 @@ object Chapter4 {
 
   def question2():Unit = {
     /**
-     *
+     *Write a program that reads words from a file. Use a mutable map. Count how often each word appears.
+     * Print out each word and their counts
      */
+    import scala.collection.mutable._
+    import java.io._
+    import java.util._
 
+    //Create file object as input to Scanner
+    val file = new File("/home/frank/sbt/learning_scala/file_operation/test.txt")
 
+    //Create a array buffer to store text for processing
+    val temp = ArrayBuffer[String]()
+
+    //Create file iterator
+    val in = new Scanner(file)
+
+    //Read file into ArrayBuffer
+    while (in.hasNext())  temp += (in.next)
+
+    //Convert ArrayBuffer to Seq for mapping
+    val mapped = temp.toSeq
+
+    //Create mutable map
+    //val words = new scala.collection.mutable.Map[String, Int]()
+
+    //Map unique strings and count
+    val words = mapped.groupBy(l => l).map(t => (t._1, t._2.length))
+
+    //Verify contents of map
+    for ((a,b) <- words) println(a,b)
 
   }
 
   def question3():Unit = {
     /**
-     *
+     *Answer question 2 using an immutable Map
      */
 
+    import java.io._
+    import java.util._
+    import scala.collection.mutable.ArrayBuffer
 
+    //Create file object as input to Scanner
+    val file = new File("/home/frank/sbt/learning_scala/file_operation/test.txt")
+
+    //Create a array buffer to store text for processing
+    val temp = ArrayBuffer[String]()
+
+    //Create file iterator
+    val in = new Scanner(file)
+
+    //Read file into ArrayBuffer
+    while (in.hasNext())  temp += (in.next)
+
+    //Convert ArrayBuffer to Seq for mapping
+    val mapped = temp.toSeq
+
+    //Create mutable map
+    var words = scala.collection.immutable.Map[String, Int]()
+
+    //Map unique strings and count
+    words = mapped.groupBy(l => l).map(t => (t._1, t._2.length))
+
+    //Verify contents of map
+    for ((a,b) <- words) println(a,b)
 
   }
 
 
   def question4():Unit = {
     /**
-     *
+     *  Same thing but sort words by alpha
      */
 
+    import scala.collection.immutable._
+    import java.io._
+    import java.util._
+    import scala.collection.mutable.ArrayBuffer
+    import scala.collection.immutable.ListMap
 
+    //Create file object as input to Scanner
+    val file = new File("/home/frank/sbt/learning_scala/file_operation/test.txt")
+
+    //Create a array buffer to store text for processing
+    val temp = ArrayBuffer[String]()
+
+    //Create file iterator
+    val in = new Scanner(file)
+
+    //Read file into ArrayBuffer
+    while (in.hasNext())  temp += (in.next)
+
+    //Convert ArrayBuffer to Seq for mapping
+    val mapped = temp.toSeq
+
+    //Create mutable map
+    var words = scala.collection.immutable.Map[String, Int]()
+
+    //Map unique strings and count
+    words = mapped.groupBy(l => l).map(t => (t._1, t._2.length))
+
+    //
+    var finalWord = scala.collection.immutable.ListMap[String,Int]()
+
+    //Sort map by words
+    finalWord = ListMap(words.toSeq.sortBy(_._1) :_*)
+
+    //Sort map by count
+    //finalWord = ListMap(words.toSeq.sortBy(_._2) :_*)
+
+
+    //Verify contents of map
+    for ((a,b) <- finalWord) println(a,b)
 
   }
 
   def question5():Unit = {
     /**
+     * Repeat the preceding exercise with a java.util.TreeMap that you adapt to the Scala API.
      *
+     * Looked into this and was able to get the words into a Java TreeMap...but producing the
+     * groupBy mapping was to complex in Java. Found several examples...none were simple enough
+     * to try to incorporate into the program above.
      */
 
 
@@ -61,7 +160,8 @@ object Chapter4 {
 
   def question6():Unit = {
     /**
-     *
+     * Define a linked hash map that maps "Monday" to java.util.Calendar.Monday. Do same for other
+     * weekdays.  Demonstrate that all elements are visited in insertion order.
      */
 
 
@@ -70,7 +170,8 @@ object Chapter4 {
 
   def question7():Unit = {
     /**
-     *
+     * Print a table of all Java properties, like this.  You ned to find the length of the longest key
+     * before you can print the table.
      */
 
 
@@ -79,7 +180,8 @@ object Chapter4 {
 
   def question8():Unit = {
     /**
-     *
+     * Write a function minmax(values: Array[Int], v: int) that returns a pair containing
+     * the smallest and largest value in an array.
      */
 
 
@@ -88,7 +190,8 @@ object Chapter4 {
 
   def question9():Unit = {
     /**
-     *
+     *  Write a function lteggt(values: Array[Int], v: int) that returns a triple containing count of
+     *  values less than v, equal to v, and greater than v.
      */
 
 
@@ -97,7 +200,8 @@ object Chapter4 {
 
   def question10():Unit = {
     /**
-     *
+     *  What happens when you zip together two strings, such as "hello".zip("World")?
+     *  Come up w a plausible use case.
      */
 
 
--- sfti_homework/src/main/scala/Maps.scala
+++ sfti_homework/src/main/scala/Maps.scala
@@ -1,27 +1,66 @@
-import java.util.Scanner
+//import scala.io.Source
+import scala.collection.immutable._
 import java.io._
+import java.util._
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.immutable.ListMap
+import java.util.TreeMap
+
+
 
 object Maps extends App {
 
   /**
-   * Write a program that reads words from a file.  Use mutable map to count how often
-   * each word appears.
+   * Write to a file
    */
-
-
- // val in = new Scanner(java.io.File("myfile.text"))
- // while (in.hasNext()) println(in.next())
-
+  /**
+    val writer = new PrintWriter(new File("/home/frank/sbt/learning_scala/file_operation/test3.txt" ))
+    writer.write("Hello Scala")
+    writer.close()
+    **/
 
   /**
-  val items = scala.collection.immutable.Map("Car" -> 10000.00, "Computer" -> 1000.00, "TV" -> 3000.00)
-  //Print initial map to verify contents
-  for ((k,v) <- items) println(k,v)
-  //Create new empty map val
-  val itemsDiscounted = scala.collection.mutable.Map[String, Double]()
-  //Add values
-  for ((a,b) <- items) itemsDiscounted += (a -> (b - (b * 0.10)))
-  //Print results
-  for ((k,v) <- itemsDiscounted) println(k,v)
+   * Read from a file
+   */
+  /**
+  println("Following is the content read:" )
+
+  Source.fromFile("/home/frank/sbt/learning_scala/file_operation/test.txt" ).foreach{
+    print
+  }
     */
+
+  //Create file object as input to Scanner
+  val file = new File("/home/frank/sbt/learning_scala/file_operation/test.txt")
+
+  //Create a array buffer to store text for processing
+  val temp = ArrayBuffer[String]()
+
+  //Create file iterator
+  val in = new Scanner(file)
+
+  //Read file into ArrayBuffer
+  while (in.hasNext())  temp += (in.next)
+
+  //Convert ArrayBuffer to Seq for mapping
+  val mapped = temp.toSeq
+
+  //Create mutable map
+  var words = TreeMap[String, Integer]()
+
+  //Map unique strings and count
+  words = mapped.groupBy(l => l).map(t => (t._1, t._2.length))
+
+  //
+  var finalWord = scala.collection.immutable.ListMap[String,Int]()
+
+  //Sort map by words
+  //finalWord = ListMap(words.toSeq.sortBy(_._1) :_*)
+
+  //Sort map by count
+  finalWord = ListMap(words.toSeq.sortBy(_._2) :_*)
+
+  //Verify contents of map
+  for ((a,b) <- finalWord) println(a,b)
+
 }
