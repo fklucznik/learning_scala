119475000bec29dbda4674b62d2d08a0e7f45af5
fklucznik <frank.klucznik@gtri.gatech.edu>
2015-02-18 22:59:08 -0500

    v21

--- sfti_homework/src/main/scala/Objects0.scala
+++ sfti_homework/src/main/scala/Objects0.scala
@@ -7,19 +7,46 @@ object Objects0 extends App {
    * gallonsToLiter
    * milesToKilometer
    */
-object Conversions {
-    private var answer = 0.0
-    def inchesToCentimeter(i: Double): Unit ={
-      answer = i/0.39370
-    }
 
-    def gallonsToLiter(g: Double): Unit = {
-      answer = g/0.26417
-    }
-
-    def milesToKilometer(m: Double): Unit = {
-      answer = m/0.62137
+  class UnitConversions{
+    def conversions(a: Double, b: String): Double = {
+      b match {
+        case "i" =>
+          UnitConversions.inchesToCentimeter(a)
+          UnitConversions.answer
+        case "g" =>
+          UnitConversions.gallonsToLiter(a)
+          UnitConversions.answer
+        case "m" =>
+          UnitConversions.milesToKilometer(a)
+          UnitConversions.answer
+      }
     }
   }
 
+
+  object UnitConversions {
+
+    private var answer = 0.0
+
+    def inchesToCentimeter(i: Double){
+      answer = i/0.39370
+      println("object1  " + answer)
+    }
+
+    def gallonsToLiter(g: Double){
+      answer = g/0.26417
+      println("object2  " + answer)
+    }
+
+    def milesToKilometer(m: Double){
+      answer = m/0.62137
+      println("object3  " + answer)
+    }
+
+  }
+
+val test = new UnitConversions
+
+println("%4.1f".format (test.conversions(5.0,"i")))
 }
